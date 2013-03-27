570a3381909a79dfb7ed242ebe1d07fa359dfa37
facelessuser <faceless.shop@gmail.com>
2013-03-26 22:45:11 -0600

    -Backport auto save to plist from JSON option from ST3 branch

--- plist_json_convert.py
+++ plist_json_convert.py
@@ -1,8 +1,8 @@
-'''
+"""
 Plist Json Converter
 Licensed under MIT
 Copyright (c) 2012 Isaac Muse <isaacmuse@gmail.com>
-'''
+"""
 
 import sublime
 import sublime_plugin
@@ -36,9 +36,24 @@ ERRORS = {
 
 def error_msg(msg, e=None):
     sublime.error_message(msg)
-    if e != None:
+    if e is not None:
         print "Plist Json Converter Exception:"
         print e
+
+
+class PlistToJsonListener(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        convert = False
+        ext2convert = sublime.load_settings(PACKAGE_SETTINGS).get("json_to_plist_on_save", [])
+        filename = view.file_name()
+        if filename is not None:
+            for end in ext2convert:
+                if filename.lower().endswith(end.lower()):
+                    convert = True
+                    break
+
+        if convert:
+            view.run_command("json_to_plist", {"save_to_file": 'True', "show_file": False, "force": True})
 
 
 class LanguageConverter(sublime_plugin.TextCommand):
@@ -47,27 +62,28 @@ class LanguageConverter(sublime_plugin.TextCommand):
     settings = None
 
     def __set_syntax(self):
-        if self.output_view != None:
+        if self.output_view is not None:
             # Get syntax language and set it
-            syntax = sublime.load_settings(self.settings).get(self.lang, self.default_lang) if self.lang != None else self.default_lang
+            syntax = sublime.load_settings(self.settings).get(self.lang, self.default_lang) if self.lang is not None else self.default_lang
             self.output_view.set_syntax_file(syntax)
 
-    def __write_file(self, edit):
+    def __write_file(self, edit, show_file):
         errors = False
         # Get current view's filename
         filename = self.view.file_name()
-        save_filename = self.get_output_file(filename) if filename != None and exists(filename) else None
+        save_filename = self.get_output_file(filename) if filename is not None and exists(filename) else None
 
-        if save_filename != None:
+        if save_filename is not None:
             # Save content to UTF file
             try:
                 with codecs.open(save_filename, "w", "utf-8") as f:
                     f.write(self.output)
-                self.output_view = self.view.window().open_file(save_filename)
+                if show_file:
+                    self.output_view = self.view.window().open_file(save_filename)
             except Exception, e:
                 errors = True
                 error_msg(ERRORS["filewrite"], e)
-            if not errors:
+            if not errors and show_file:
                 self.__set_syntax()
         else:
             # Could not acquire a name that exists on disk
@@ -94,17 +110,18 @@ class LanguageConverter(sublime_plugin.TextCommand):
             if new_buffer or force_new_buffer:
                 # If a name can be acquired from the original view, give buffer a modified derivative of the name
                 filename = self.view.file_name()
-                buffer_name = basename(self.get_output_file(filename)) if filename != None else None
-                if buffer_name != None:
+                buffer_name = basename(self.get_output_file(filename)) if filename is not None else None
+                if buffer_name is not None:
                     self.output_view.set_name(buffer_name)
             self.__set_syntax()
 
-    def is_enabled(self, save_to_file=False):
+    def is_enabled(self, save_to_file=False, force=False):
         enabled = True
-        if save_to_file and not bool(sublime.load_settings(self.settings).get("enable_save_to_file_commands", False)):
-            enabled = False
-        elif not save_to_file and not bool(sublime.load_settings(self.settings).get("enable_show_in_buffer_commands", False)):
-            enabled = False
+        if force:
+            if save_to_file and not bool(sublime.load_settings(self.settings).get("enable_save_to_file_commands", False)):
+                enabled = False
+            elif not save_to_file and not bool(sublime.load_settings(self.settings).get("enable_show_in_buffer_commands", False)):
+                enabled = False
         return enabled
 
     def get_output_file(self, filename):
@@ -116,11 +133,11 @@ class LanguageConverter(sublime_plugin.TextCommand):
     def convert(self, edit):
         return False
 
-    def run(self, edit, save_to_file=False):
+    def run(self, edit, save_to_file=False, show_file=True, **kwargs):
         if not self.read_buffer():
             if not self.convert(edit):
                 if save_to_file:
-                    self.__write_file(edit)
+                    self.__write_file(edit, show_file)
                 else:
                     self.__write_buffer(edit)
 
@@ -137,12 +154,12 @@ class PlistToJsonCommand(LanguageConverter):
         ext_tbl = sublime.load_settings(self.settings).get("conversion_ext", [])
         for ext in ext_tbl:
             m = re.match("^(.*)\\." + ext["plist"] + "$", filename, re.IGNORECASE)
-            if m != None:
+            if m is not None:
                 name = m.group(1) + "." + ext["json"]
                 break
 
         # Could not find ext in table, replace current extension with default
-        if name == None:
+        if name is None:
             name = splitext(filename)[0] + ".JSON"
         return name
 
@@ -188,12 +205,12 @@ class JsonToPlistCommand(LanguageConverter):
         ext_tbl = sublime.load_settings(self.settings).get("conversion_ext", [])
         for ext in ext_tbl:
             m = re.match("^(.*)\\." + ext["json"] + "$", filename, re.IGNORECASE)
-            if m != None:
+            if m is not None:
                 name = m.group(1) + "." + ext["plist"]
                 break
 
         # Could not find ext in table, replace current extension with default
-        if name == None:
+        if name is None:
             name = splitext(filename)[0] + ".plist"
         return name
 
